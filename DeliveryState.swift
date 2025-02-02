732e690d61977ea49d195fddd48094cea48b8498
2hayeon2 <150683972+2hayeon2@users.noreply.github.com>
2025-02-02 23:02:13 +0900

    Update DeliveryState.swift

--- DeliveryState.swift
+++ DeliveryState.swift
@@ -4,19 +4,4 @@ enum DeliveryState {
     case inTransit        // 배송 중
     case delivered        // 배송 완료
     case returned         // 반품 완료
-    
-    func isCancelAvailable(by policy: PurchaseCancelPolicy) -> Bool {
-        let states = self.getCancelAvailableStates(by: policy)
-        return states.contains(self)
-    }
-    
-    private func getCancelAvailableStates(by policy: PurchaseCancelPolicy) -> [DeliveryState] {
-        if policy is FreshGroceriesCancelPolicy {
-            return [.preparing]
-        }
-        if policy is ElectronicsCancelPolicy {
-            return [.preparing, .shipped, .inTransit, .delivered]
-        }
-        return []
-    }
 }
