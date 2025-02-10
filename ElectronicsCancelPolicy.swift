6dab6f61e7e8e1d0ab59570d3b0dbd4ed9fc566c
2hayeon2 <150683972+2hayeon2@users.noreply.github.com>
2025-02-10 21:57:19 +0900

    Update ElectronicsCancelPolicy.swift

--- ElectronicsCancelPolicy.swift
+++ ElectronicsCancelPolicy.swift
@@ -6,11 +6,13 @@ class ElectronicsCancelPolicy: PurchaseCancelPolicy {
     var cancelAvailableDeliveryStates: [DeliveryState] {
         return [.preparing, .shipped, .inTransit, .delivered]
     }
+
+    let deadline: Int
 	
-    internal var deadline: Int {
-        return 31
+    init(deadline: Int) {
+	self.deadline = deadline
     }
-    
+
     func isCancelAvailable(_ purchase: PurchaseHistory) -> Bool {
         let passedDays = Calendar.current.dateComponents([.day], from: purchase.purchaseDate, to: Date()).day ?? 0
         return cancelAvailablePurchaseStates.contains(purchase.purchaseState) &&
